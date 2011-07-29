860c279808425d718bbb721d36884dded6af26eb
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-07-28 21:44:46 -0500

    Refactor tests and add compressing test

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
+require "sprockets/railtie"
 
 Bundler.require
 require "uglifier-rails"
--- test/uglifier_rails_test.rb
+++ test/uglifier_rails_test.rb
@@ -1,12 +1,25 @@
 require 'test_helper'
 
 class UglifierRailsTest < ActiveSupport::TestCase
+  include Rack::Test::Methods
+
+  def app
+    @app ||= Rails.application
+  end
+
   test "classes are loaded" do
     assert_kind_of Module, Uglifier::Rails
     assert_kind_of Class, Uglifier::Rails::Railtie
   end
 
   test "js compressor is set" do
-    assert_kind_of Uglifier, Dummy::Application.config.assets.js_compressor
+    assert_equal :uglifier, Rails.application.config.assets.js_compressor
+    assert_not_nil Rails.application.assets.js_compressor
+  end
+
+  test "js files are compressed" do
+    get "/assets/test.js"
+
+    assert_match "function test(){alert(\"Hello World\")}", last_response.body
   end
 end
